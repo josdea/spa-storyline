5b814b9076656480d51280baaf06d2214327a79a
Josh Dean <josdea@gmail.com>
2022-02-11 11:29:25 -0500

    root start of dist folder

---
+++ precache-manifest.c86edb5263c67548b1606c2458c86ad4.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67727bbdb629f620891f",
+    "url": "/css/app.1e251b4b.css"
+  },
+  {
+    "revision": "7831fd64131c9ea307e63643f61d508f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67727bbdb629f620891f",
+    "url": "/js/app.5c6a365d.js"
+  },
+  {
+    "revision": "034e362b69db7de5ff52",
+    "url": "/js/chunk-vendors.7761050f.js"
+  },
+  {
+    "revision": "ba15a9460ab7da7402ed5268e69d15ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "720f8fd109feb2d463d8870da8d58c54",
+    "url": "/story/analytics-frame.html"
+  },
+  {
+    "revision": "731c744b69d9b4e4ada2fd0c03aaa21b",
+    "url": "/story/html5/data/css/output.min.css"
+  },
+  {
+    "revision": "5144a5e4c08176a83901bca868bfb410",
+    "url": "/story/html5/data/js/5nsoRJNmksU.js"
+  },
+  {
+    "revision": "c0bf000fbacc3365c44ec0dcd590aa74",
+    "url": "/story/html5/data/js/6BUm170eUnj.js"
+  },
+  {
+    "revision": "ce0f90408815d270ff8c49c5e330bb18",
+    "url": "/story/html5/data/js/6ODsJ2UTOUB.js"
+  },
+  {
+    "revision": "c63e3b6c3616601cea6756c647044455",
+    "url": "/story/html5/data/js/6RRlXEUrVeu.js"
+  },
+  {
+    "revision": "c6352b3fbf671902dca267893a78608a",
+    "url": "/story/html5/data/js/6XCzsS31H6a.js"
+  },
+  {
+    "revision": "5f71c2f00a64bbb6e265dfdc410767dc",
+    "url": "/story/html5/data/js/data.js"
+  },
+  {
+    "revision": "d021d2f042a8fb0a601168bc1663546f",
+    "url": "/story/html5/data/js/frame.js"
+  },
+  {
+    "revision": "c40f06b5006936f240e8d32392ae1bb3",
+    "url": "/story/html5/data/js/paths.js"
+  },
+  {
+    "revision": "0b17c9d99720f9d09032e0fa42d9649d",
+    "url": "/story/html5/lib/scripts/bootstrapper.min.js"
+  },
+  {
+    "revision": "e56eafed1067835e54825db06c4d8102",
+    "url": "/story/html5/lib/scripts/frame.desktop.min.js"
+  },
+  {
+    "revision": "b1065babdb13463114b2cf77d83d5932",
+    "url": "/story/html5/lib/scripts/frame.mobile.min.js"
+  },
+  {
+    "revision": "226549a59d44ca451ab42097a60b9134",
+    "url": "/story/html5/lib/scripts/slides.min.js"
+  },
+  {
+    "revision": "2c2db9678aa7a8ec0c44799380d05036",
+    "url": "/story/html5/lib/stylesheets/desktop.min.css"
+  },
+  {
+    "revision": "72862e7cf19603ad24f26baf86dd0e08",
+    "url": "/story/html5/lib/stylesheets/mobile-fonts/open-sans-bold.woff"
+  },
+  {
+    "revision": "0d0d7107450f05b72a4507d0d7687dd1",
+    "url": "/story/html5/lib/stylesheets/mobile-fonts/open-sans-light.woff"
+  },
+  {
+    "revision": "ce659615885f33d928eb7fe276574106",
+    "url": "/story/html5/lib/stylesheets/mobile-fonts/open-sans-regular.woff"
+  },
+  {
+    "revision": "b18e6b636482b45ebc23b45f2e7cd906",
+    "url": "/story/html5/lib/stylesheets/mobile.min.css"
+  },
+  {
+    "revision": "7e5e4acb4ae5c5d88e84b9d95241539a",
+    "url": "/story/meta.xml"
+  },
+  {
+    "revision": "45a24ce1a00388cad8a153315a4b6bb4",
+    "url": "/story/mobile/6JuDp9msvGX_DX1684_DY1684_CX1263_CY842.jpg"
+  },
+  {
+    "revision": "895454c2edb85636237bee36a17189e3",
+    "url": "/story/mobile/poster_5cwmHi6KLSK_video_6BRrBxhCXqU_18_160_790x444.jpg"
+  },
+  {
+    "revision": "895454c2edb85636237bee36a17189e3",
+    "url": "/story/mobile/poster_5neS2TPfTcm_video_6BRrBxhCXqU_18_160_790x444.jpg"
+  },
+  {
+    "revision": "895454c2edb85636237bee36a17189e3",
+    "url": "/story/mobile/poster_65Gx3NGHrES_video_6BRrBxhCXqU_18_160_790x444.jpg"
+  },
+  {
+    "revision": "895454c2edb85636237bee36a17189e3",
+    "url": "/story/mobile/poster_6JStVJabrvH_video_6BRrBxhCXqU_18_160_790x444.jpg"
+  },
+  {
+    "revision": "895454c2edb85636237bee36a17189e3",
+    "url": "/story/mobile/poster_6dhibX1B18S_video_6BRrBxhCXqU_18_160_790x444.jpg"
+  },
+  {
+    "revision": "2d36b82599ac26a209205f181f3106c5",
+    "url": "/story/story.html"
+  },
+  {
+    "revision": "eaac3cc7c5f51a1783e4ff4a210f6ea0",
+    "url": "/story/story_content/thumbnail.jpg"
+  },
+  {
+    "revision": "a85671704f0073177c1110ad8d5ff5ae",
+    "url": "/story/story_content/user.js"
+  },
+  {
+    "revision": "541146aa314b019f18ce5c41cdf34416",
+    "url": "/story/story_content/video_6BRrBxhCXqU_18_160_790x444.mp4"
+  }
+]);
